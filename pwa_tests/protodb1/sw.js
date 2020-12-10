b94407c32ae626bc29628514218cbfb7f8ab2645
leeharr <missive@hotmail.com>
2020-12-10 15:20:41 -0500

    set up offline cache

--- pwa_tests/protodb1/sw.js
+++ pwa_tests/protodb1/sw.js
@@ -1,24 +1,72 @@
-var cacheName = 'protodb';
+/* SW-V005 */
+
+var cacheName = 'protodb005';
 var filesToCache = [
-/*  'index.html', */
-/*  'static/css/style.css', */
-  'static/js/main.js'
+  './',
+  'index.html',
+  'admin.html',
+
+  'static/css/style.css',
+
+  'static/js/main.js',
+  'static/js/knockout-3.5.1.js',
+  'static/js/idb-keyval-iife.js',
+  'static/js/protodb.js',
+  'static/js/protodb-admin.js',
+
+  'static/js/session_questions.json',
+  'static/js/person_questions.json',
+
+  'static/images/favicon.ico',
+  "static/images/protodb-icon-128.png",
+  "static/images/protodb-icon-144.png",
+  "static/images/protodb-icon-152.png",
+  "static/images/protodb-icon-192.png",
+  "static/images/protodb-icon-256.png",
+  "static/images/protodb-icon-512.png",
 ];
 
-/* Start the service worker and cache all of the app's content */
+/* on install, get fresh version from server */
+var nocacheRequest = function(url){
+    r = new Request(url, {'cache': 'reload'});
+    return r;
+}
+
+
+/* Install the service worker and cache everything needed offline */
 self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(filesToCache);
-    })
-  );
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            var ncrs = filesToCache.map(nocacheRequest);
+            return cache.addAll(ncrs);
+        })
+    );
+});
+
+/* Remove old caches when new service worker activated */
+self.addEventListener('activate', function(event){
+    event.waitUntil(
+        caches.keys().then(function(cacheNames){
+            return Promise.all(
+                cacheNames
+                .filter(function(cname){
+                    // Return true if you want to remove this cache
+                    // (all but the current, which is cacheName)
+                    return cname != cacheName;
+                })
+                .map(function (cname) {
+                    return caches.delete(cname);
+                }),
+            );
+        }),
+    );
 });
 
 /* Serve cached content when offline */
 self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request);
+        })
+    );
 });
