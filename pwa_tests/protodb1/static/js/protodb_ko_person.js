54542366638df9b4ab420095714f139cce5a7495
leeharr <missive@hotmail.com>
2021-09-13 13:20:25 -0400

    rmv debug

--- pwa_tests/protodb1/static/js/protodb_ko_person.js
+++ pwa_tests/protodb1/static/js/protodb_ko_person.js
@@ -9,11 +9,8 @@ var Person = function(pid, lname, fname, grade){
     self.grade = ko.observable(grade);
 
     self.checkorshowperson = async function(){
-        console.log('CoSP');
         if (vm.showpersoncheckboxes()){
-            console.log('-- Check');
             let ngpid = '#ngpid'+self.pid();
-            console.log('-- '+ngpid);
             let cb = document.querySelector(ngpid);
             if (cb.checked){
                 cb.checked = false;
@@ -21,44 +18,34 @@ var Person = function(pid, lname, fname, grade){
                 cb.checked = true;
             }
         } else {
-            console.log('-- Showperson');
             await self.showperson();
         }
     }
 
     self.showperson = async function(){
-        console.log('SP');
         vm.shownewperson(true);
         vm.personexists(true);
         if (vm.selectedgroup()){
             vm.selectedgroup().savecancel();
         }
 
-        console.log(self.pid());
         let dbp = await pget(self.pid());
 
         let school = document.getElementById('newperson_questionsschool');
-        console.log(dbp.schoolid);
         school.value = dbp.schoolid;
 
-        console.log(self.fname());
-        console.log(dbp.fname);
         let fn = document.getElementById('newperson_questionsfname');
         fn.value = dbp.fname;
 
-        console.log(self.lname());
         let ln = document.getElementById('newperson_questionslname');
         ln.value = dbp.lname;
 
-        console.log(self.grade());
         let gr = document.getElementById('newperson_questionsgrade');
         gr.value = dbp.gradeid;
 
-        console.log('DOB');
         let dob = document.getElementById('newperson_questionsdob');
         dob.value = dbp.dob;
 
-        console.log(self.pid());
         let xp = document.getElementById('xpersonid');
         xp.value = self.pid();
     }
