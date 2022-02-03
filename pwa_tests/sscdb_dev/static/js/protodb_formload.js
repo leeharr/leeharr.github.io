05126d5532ad065d64c1b7045f2848c3eed56b1b
leeharr <missive@hotmail.com>
2022-02-03 10:56:20 -0500

    int input per person

--- pwa_tests/sscdb_dev/static/js/protodb_formload.js
+++ pwa_tests/sscdb_dev/static/js/protodb_formload.js
@@ -96,6 +96,7 @@ window.intinputper = function(div, req){
     div.appendChild(ti);
 
     let t = document.createElement('table');
+    t.classList.add('bordered');
     div.appendChild(t);
     div.thesubtab = t;
 
--- pwa_tests/sscdb_dev/sw.js
+++ pwa_tests/sscdb_dev/sw.js
@@ -1,4 +1,4 @@
-var cacheName = 'sscdb_devAL09';
+var cacheName = 'sscdb_devAL10';
 var filesToCache = [
   './',
   'index.html',
