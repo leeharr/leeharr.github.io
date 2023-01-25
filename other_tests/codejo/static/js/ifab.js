239aa2b55fc0ee145bfa91cb0fc1c9ba24850541
leeharr <missive@hotmail.com>
2023-01-25 11:18:37 -0500

    S obj

--- other_tests/codejo/static/js/ifab.js
+++ other_tests/codejo/static/js/ifab.js
@@ -1,2 +1,19 @@
 var c = new fabric.Canvas('c');
 c.backgroundColor = 'lightgray';
+c.renderAll();
+
+var S = function(side){
+    let ns = new fabric.Rect({
+        width: side,
+        height: side,
+        top: 100,
+        left: 100,
+        fill: 'red',
+        selectable: false,
+        evented: false,
+    })
+
+    c.add(ns);
+    c.renderAll();
+    return ns;
+}
