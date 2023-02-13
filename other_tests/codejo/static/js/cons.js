b14c5917676445418ee482788fb33c0ef4df579a
leeharr <missive@hotmail.com>
2023-02-13 11:45:06 -0500

    cons

--- other_tests/codejo/static/js/cons.js
+++ other_tests/codejo/static/js/cons.js
@@ -1,5 +1,9 @@
 const cons = document.querySelector('#cons');
 
 cons.onkeyup = function(ev){
-    console.log('coku : ' + ev);
+    console.log('coku : ' + ev.key);
+}
+
+cons.onclick = function(ev){
+    console.log('cclk');
 }
