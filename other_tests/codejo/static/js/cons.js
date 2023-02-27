e7944e5bb47780625f78fd92da1a976525d5c0be
leeharr <missive@hotmail.com>
2023-02-27 10:56:43 -0500

    curpos

--- other_tests/codejo/static/js/cons.js
+++ other_tests/codejo/static/js/cons.js
@@ -1,11 +1,11 @@
 const cons = document.querySelector('#cons');
 
 cons.onkeypress = function(ev){
-    let pos = cons.prop('selectionStart');
+    let pos = cons.selectionStart;
     console.log('cokp : ' + ev.key + ':' + pos);
 }
 
 cons.onclick = function(ev){
-    let pos = cons.prop('selectionStart');
+    let pos = cons.selectionStart;
     console.log('cclk :' + pos);
 }
