a5f872251ccc35170337ee0f52f27be7ff39935b
leeharr <missive@hotmail.com>
2023-02-28 11:36:43 -0500

    lineno

--- other_tests/codejo/static/js/cons.js
+++ other_tests/codejo/static/js/cons.js
@@ -1,13 +1,15 @@
 const cons = document.querySelector('#cons');
 
 var lineno = 1;
+var lines = {};
 
 cons.onkeypress = function(ev){
     if (ev.key == 'Enter'){
         lineno += 1;
     }
     let pos = cons.selectionStart;
-    console.log('cokp : ' + ev.key + ':' + pos + '|lineno:'+lineno+'|');
+    console.log('cokp : ' + ev.key + ':' + pos + ' cols:'+cons.cols+ ' rows: '+cons.rows+ '|lineno:'+lineno+'|');
+    console.log(cons.val());
 }
 
 cons.onclick = function(ev){
