50e495706a964a8b29ee07bed047cb19e6b93c0f
leeharr <missive@hotmail.com>
2023-03-02 11:29:36 -0500

    enter

--- other_tests/codejo/static/js/cons.js
+++ other_tests/codejo/static/js/cons.js
@@ -17,6 +17,9 @@ cons.onkeydown = function(ev){
 cons.onkeypress = function(ev){
     if (ev.key == 'Enter'){
         lineno += 1;
+        cons.value += '\nJS>';
+        ev.preventDefault();
+        return
     }
     let pos = cons.selectionStart;
     console.log('cokp : ' + ev.key + ':' + pos + ' cols:'+cons.cols+ ' rows: '+cons.rows+ '|lineno:'+lineno+'|');
