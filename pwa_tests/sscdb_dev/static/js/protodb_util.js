1edfa8144447e3367e7bd608b7230caab81835f6
leeharr <missive@hotmail.com>
2023-09-15 11:13:46 -0400

    purge sessions

--- pwa_tests/sscdb_dev/static/js/protodb_util.js
+++ pwa_tests/sscdb_dev/static/js/protodb_util.js
@@ -267,8 +267,8 @@ var purge_old_sessions = async function(){
     let dt = new Date();
     let dstr = [
             dt.getFullYear(),
-            ('0' + (d.getMonth() + 1)).slice(-2),
-            ('0' + d.getDate()).slice(-2)
+            ('0' + (dt.getMonth() + 1)).slice(-2),
+            ('0' + dt.getDate()).slice(-2)
         ].join('-');
     await cset('purged', dstr);
 }
--- pwa_tests/sscdb_dev/sw.js
+++ pwa_tests/sscdb_dev/sw.js
@@ -1,4 +1,4 @@
-var cacheName = 'sscdb_devCP06';
+var cacheName = 'sscdb_devCP07';
 var filesToCache = [
   './',
   'index.html',
